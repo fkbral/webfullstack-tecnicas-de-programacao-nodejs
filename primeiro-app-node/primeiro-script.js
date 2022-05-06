3deb54410bb09f10288966fc979e79acc22b7b5f
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-05-06 20:45:52 -0300

    criação do repositório

---
+++ primeiro-app-node/primeiro-script.js
@@ -0,0 +1,27 @@
+const alunos = ["Fulano", "Maria", "Zeca"]
+
+alunos.forEach(aluno => {
+  console.log(`Boa noite, ${aluno}`)
+})
+
+
+// Window não está está disponível no node: dá erro
+try {
+  window.alert('Não entro aqui, pois não existo no node')
+  alert('Não entro aqui, pois não existo no node')
+} catch (error) {
+  // console.error(error)
+  console.error('Global Window não está disponível no node')
+}
+
+try {
+  const numeroDigitadoPeloUsuario = parseInt(prompt('Entre com um número: '))
+  console.log(numeroDigitadoPeloUsuario)
+} catch (error) {
+  console.error(
+    'Global Window não está disponível no node, logo prompt não pode ser usado'
+  )
+}
+
+const numero = 10
+console.log(numero)
