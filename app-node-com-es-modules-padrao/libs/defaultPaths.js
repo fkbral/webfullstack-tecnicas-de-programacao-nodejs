fa453f0fcfd61cd4b1fb08d4dffa9a40899ab2ee
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-05-11 22:30:02 -0300

    fix: aula 03 - windows __dirname e __filename

--- app-node-com-es-modules-padrao/libs/defaultPaths.js
+++ app-node-com-es-modules-padrao/libs/defaultPaths.js
@@ -2,16 +2,17 @@ import path from 'path'
 import os from 'os'
 
 export function createDirAndFileNameGlobals(importPath) {
-  const osName = os.type()
-  let [, __filename] = importPath
-  .split('file://')
-
-  if(osName === 'Windows_NT') {
-    __filename = __filename.split(':/')?.[1]
+  const splitByHashMap = {
+    'Linux': 'file://',
+    'Darwin': 'file://',
+    'Windows_NT': 'file:///',
   }
-
+  
+  const osName = os.type()
+  const splitByString = splitByHashMap[osName]
+  const [, __filename] = importPath.split(splitByString)
   const __dirname = path.join(__filename, '..')
-
+  
   return {
     __dirname,
     __filename
