92b23bd9d7ee776073e42ca5b2024cf1c7dbc732
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-05-18 21:15:50 -0300

    chore: aula 06 - incluindo comentários sobre código de DNS

--- node-com-typescript/04_dns.ts
+++ node-com-typescript/04_dns.ts
@@ -4,16 +4,21 @@ import { Resolver } from 'dns/promises'
 async function pesquisaDNS() {
   const urlPesquisada = 'letscode.com.br'
 
+  // Realizando uma pesquisa pelo ip do domínimo letscode.com.br usando provedores de DNS padrão
   console.time('busca de ipsv4')
   const ipsv4 = await dns.promises.resolve4(urlPesquisada)
   console.timeEnd('busca de ipsv4')
   console.log(ipsv4)
+
+  // Descobrindo provedor de DNS usado pelo site
   const provedoresDeDNS = await dns.promises.resolveNs(urlPesquisada)
   console.log(provedoresDeDNS)
 
+  // Resgatando IP de provedor de DNS usado pelo site
   const ipsv4NS = await dns.promises.resolve4(provedoresDeDNS[0])
   console.log(ipsv4NS)
 
+  // Criando novo resolvedor de DNS com os IPs dos provedores de DNS usados pelo site
   const dnsResolverDaAWS = new Resolver()
   dnsResolverDaAWS.setServers([ipsv4NS[0]])
 
